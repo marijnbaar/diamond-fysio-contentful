b7038287acd2960213c43319d5bca0b601559a6b
Marc Selles <mselles94@gmail.com>
2022-03-20 15:07:58 +0100

    Structure of contentful connection and clean-up

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const prettierCommand = (filenames) =>
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx,css,md}': [prettierCommand]
+};
