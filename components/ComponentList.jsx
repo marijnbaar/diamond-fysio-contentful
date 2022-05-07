12263e3fb132bd92419f501687e64927fccef7c6
Marijn Baar <marijn@bikkelhart.com>
2022-05-07 16:06:27 +0200

    added testimonial component query

--- components/ComponentList.jsx
+++ components/ComponentList.jsx
@@ -2,6 +2,7 @@ import HomepageHeader from './Headers/HomepageHeader';
 import Highlight from './Highlight';
 import Specialisations from './Specialisations';
 import Info from './Info';
+import Testimonial from './Testimonial';
 
 const ComponentList = ({ components }) =>
   components.map((component) => {
@@ -18,6 +19,9 @@ const ComponentList = ({ components }) =>
         }
         case 'Info': {
           return <Info key={component.sys.id} id={component.sys.id} {...component} />;
+        }
+        case 'Testimonial': {
+          return <Testimonial key={component.sys.id} id={component.sys.id} {...component} />;
         }
       }
     }
--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -3,10 +3,7 @@ export default function Testimonial() {
     <div className="pb-16 bg-gradient-to-r from-teal-500 to-cyan-600 lg:pb-0 lg:z-10 lg:relative">
       <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
         <div className="relative lg:-my-8">
-          <div
-            aria-hidden="true"
-            className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
-          />
+          <div aria-hidden="true" className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
           <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
             <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
               <img
@@ -30,16 +27,13 @@ export default function Testimonial() {
                   <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                 </svg>
                 <p className="mt-6 text-2xl font-medium text-white">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  urna nulla vitae laoreet augue. Amet feugiat est integer dolor
-                  auctor adipiscing nunc urna, sit.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae
+                  laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
                 </p>
               </div>
               <footer className="mt-6">
                 <p className="text-base font-medium text-white">Iva Lesic</p>
-                <p className="text-base font-medium text-cyan-100">
-                  Fysiotherapeut
-                </p>
+                <p className="text-base font-medium text-cyan-100">Fysiotherapeut</p>
               </footer>
             </blockquote>
           </div>
