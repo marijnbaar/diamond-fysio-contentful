0d8f4c255e7d2c50dd6eda9554af2e44002286e3
Marijn Baar <marijn@bikkelhart.com>
2022-06-12 21:09:07 +0200

    added specialisation pages header and text component

--- components/Specialisation.jsx
+++ components/Specialisation.jsx
@@ -2,27 +2,31 @@ import React from 'react';
 import Image from 'next/image';
 import setRichtTextToReactComponents from '../lib/helpers/setRichTextToReactComponents';
 
-const Specialisation = ({ image, description }) => {
+const Specialisation = ({ image, description, subtitle }) => {
   return (
     <>
-      <div className="bg-blue-extraLight relative">
+      <div className=" bg-grey-light relative">
         <div className="mx-auto">
           <div
             className="lg:gap-y-11 pt-6'
-            flex flex-col lg:grid lg:grid-cols-12 z-10 relative lg:pb-32 xl:pb-48"
+            flex flex-col lg:grid lg:grid-cols-12 z-10 relative"
           >
-            <div className="order-3 translate-y-5 lg:translate-y-0 order-0 mb-4 lg:order-none lg:col-span-5 relative">
-              <div className="z-10 h-[455px] xl-[355px] -translate-x-4 w-[calc(100%+16px)] sm:translate-x-0 sm:w-full relative lg:absolute lg:w-[calc(100%+32px+(100vw-992px)/2)] lg:left-[calc(-1*(32px+(100vw-992px)/2))] xl:w-[calc(100%+32px+(100vw-1184px)/2)] xl:left-[calc(-1*(32px+(100vw-1184px)/2))] 2xl:w-[calc(100%+32px+(100vw-1472px)/2)] 2xl:left-[calc(-1*(32px+(100vw-1472px)/2))]">
+            <div className="order-3 lg:translate-y-0 order-0 lg:order-none lg:col-span-5 relative">
+              <div className="z-10 h-[580px] xl-[355px] w-full relative">
                 {image && (
                   <Image src={image.url} alt={image.description} layout="fill" objectFit="cover" />
                 )}
               </div>
             </div>
-            <div className="relative lg:mt-0 p-11 lg:col-span-6">
+            <div className="relative lg:mt-0 p-4 lg:py-11 lg:px-14 lg:col-span-6 text-left ">
+              <h2 className="pt-6 pb-2 text-base font-semibold tracking-wider text-cyan-600 uppercase">
+                {subtitle && subtitle}
+              </h2>
+
               {description && (
-                <div className="pb-4 lg:pb-24 text-base text-gray-500">
+                <p className="pb-4 mt-2 lg:pb-24 text-base text-gray-500">
                   {setRichtTextToReactComponents(description.json)}
-                </div>
+                </p>
               )}
             </div>
           </div>
