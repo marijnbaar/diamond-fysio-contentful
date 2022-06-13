07bf9c7c31b52b2c9da25c4d545107a2679f2896
Marijn Baar <marijn@bikkelhart.com>
2022-06-13 13:16:49 +0200

    fixed build issue

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -16,7 +16,7 @@ const Page = (pageProps) => {
 };
 
 export const getStaticProps = async ({ params, preview = false }) => {
-  const slug = `/${params.slug}`;
+  const slug = `/${params.slug.join('/')}`;
   const modelId = await getTypeName(slug, preview, queryAllPages);
   const pageData = (await getPage(modelId, slug, preview)) ?? [];
   return {
