fa31cc6e7174fecd042cc8fcce2cbe4c37822ee4
Marijn Baar <marijn@bikkelhart.com>
2022-06-20 18:33:14 +0200

    refactoring

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Menu from '../components/Menu';
+// import Menu from '../components/Menu';
 
 export default function about() {
   return (
     <div className="cover">
-      <Menu />
+      {/* <Menu /> */}
       <img
         className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-[82vh]"
         src="https://images.pexels.com/photos/5473182/pexels-photo-5473182.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
