d43ec12be7203a087bb2f97a8ce6ea64286993f3
Marijn Baar <marijn@bikkelhart.com>
2022-06-24 14:47:35 +0200

    added image to header aboutpage

--- components/Headers/AboutpageHeader.jsx
+++ components/Headers/AboutpageHeader.jsx
@@ -1,16 +1,16 @@
-const image = [
-  {
-    url: 'https://images.pexels.com/photos/5473182/pexels-photo-5473182.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  }
-];
+// const image = [
+//   {
+//     url: 'https://images.pexels.com/photos/5473182/pexels-photo-5473182.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+//   }
+// ];
 
-const AboutpageHeader = () => {
+const AboutpageHeader = ({ image }) => {
   return (
     <div>
       <img
         className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-[82vh]"
-        src={image[0].url}
-        alt={image[0].description}
+        src={image.url}
+        alt={image.description}
       />
     </div>
   );
