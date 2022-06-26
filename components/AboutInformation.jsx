9a2fc25e115324cfcd5be42ad4f453e87cddd5c2
Marijn Baar <marijn@bikkelhart.com>
2022-06-26 19:56:21 +0200

    added feature aboutpage with button

--- components/AboutInformation.jsx
+++ components/AboutInformation.jsx
@@ -1,40 +1,8 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Button from './Button';
+import createSlug from '../lib/helpers/createSlug';
 
-import {
-  AnnotationIcon,
-  GlobeAltIcon,
-  LightningBoltIcon,
-  ScaleIcon
-} from '@heroicons/react/outline';
-
-const features = [
-  {
-    name: 'Competitive exchange rates',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: GlobeAltIcon
-  },
-  {
-    name: 'No hidden fees',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: ScaleIcon
-  },
-  {
-    name: 'Transfers are instant',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: LightningBoltIcon
-  },
-  {
-    name: 'Mobile notifications',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: AnnotationIcon
-  }
-];
-
-export default function AboutInformation({ title, subtitle, description }) {
+export default function AboutInformation({ title, subtitle, description, aboutFeatureCollection }) {
   return (
     <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -53,19 +21,38 @@ export default function AboutInformation({ title, subtitle, description }) {
         </div>
         <div className="mt-10">
           <dl className="space-y-10 mx-auto md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-beige text-white">
-                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+            {aboutFeatureCollection &&
+              aboutFeatureCollection.items.map((feature) => (
+                <div key={feature.title} className="relative">
+                  <dt>
+                    <p className="text-center lg:ml-24 text-lg leading-6 font-medium text-gray-900">
+                      {feature.title}
+                    </p>
+                  </dt>
+                  <dd className="mt-2 text-center lg:ml-24 text-base text-gray-500">
+                    {feature.description}
+                  </dd>
+                  <div className="m-auto lg:w-96 lg:ml-36">
+                    {feature.button && (
+                      <div className="my-2">
+                        <Button
+                          title={feature.button.title}
+                          type={feature.button.type}
+                          internal_link={
+                            feature.button.internalLink &&
+                            createSlug(
+                              feature.button.internalLink.slug,
+                              feature.button.internalLink.__typename
+                            )
+                          }
+                          external_link={feature.button.externalLink}
+                          extra_classes="sm:w-auto"
+                        />
+                      </div>
+                    )}
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    {feature.name}
-                  </p>
-                </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
-              </div>
-            ))}
+                </div>
+              ))}
           </dl>
         </div>
       </div>
