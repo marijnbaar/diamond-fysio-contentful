ffac38d954363d8faccbcff9389883c549a1a3e6
Marijn Baar <marijn@bikkelhart.com>
2022-06-26 20:03:04 +0200

    added second button aboutpage

--- components/AboutInformation.jsx
+++ components/AboutInformation.jsx
@@ -23,16 +23,16 @@ export default function AboutInformation({ title, subtitle, description, aboutFe
           <dl className="space-y-10 mx-auto md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
             {aboutFeatureCollection &&
               aboutFeatureCollection.items.map((feature) => (
-                <div key={feature.title} className="relative">
+                <div key={feature.title} className="relative p-6">
                   <dt>
-                    <p className="text-center lg:ml-24 text-lg leading-6 font-medium text-gray-900">
+                    <p className="text-center lg:ml-2 text-lg leading-6 font-medium text-gray-900">
                       {feature.title}
                     </p>
                   </dt>
-                  <dd className="mt-2 text-center lg:ml-24 text-base text-gray-500">
+                  <dd className="mt-2 text-center lg:ml-2 text-base text-gray-500">
                     {feature.description}
                   </dd>
-                  <div className="m-auto lg:w-96 lg:ml-36">
+                  <div className="m-auto lg:w-96">
                     {feature.button && (
                       <div className="my-2">
                         <Button
