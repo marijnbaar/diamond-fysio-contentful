8f6598ad38c0025ec6e9424b2a43929a3e77b0f4
Marijn Baar <marijn@bikkelhart.com>
2022-06-27 15:37:11 +0200

    added tags to teammember overview

--- components/Teammember.jsx
+++ components/Teammember.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Popover } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/solid';
 
 function Teammember({ teamMemberCollection }) {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 ">
@@ -26,14 +31,44 @@ function Teammember({ teamMemberCollection }) {
                   )}
                   <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                     <div className="flex-1">
-                      <div className="text-sm font-medium text-cyan-600">
-                        <div className="hover:underline">{person.specialisations}</div>
+                      <div className="text-sm font-small flex flex-row flex-wrap text-white">
+                        {person.specialisationTagsCollection.items.map((specialisation) => (
+                          <div
+                            key={specialisation.tag}
+                            className="bg-beige hover:bg-gray-400 p-1 m-1 rounded"
+                          >
+                            {specialisation.tag}
+                          </div>
+                        ))}
                       </div>
                       <div className="block mt-2">
-                        <p className="text-xl font-semibold text-gray-900">{person.name}</p>
-                        <p className="mt-3 text-base text-gray-500">
-                          {person.description && person.description}
-                        </p>
+                        <p className="text-xl font-semibold text-gray-900 mb-2">{person.name}</p>
+                        <Popover>
+                          {({ open }) => (
+                            <>
+                              <Popover.Button
+                                className={classNames(
+                                  open ? 'text-gray-900' : 'text-gray-500',
+                                  'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-beige'
+                                )}
+                              >
+                                <span>Lees meer</span>
+                                <ChevronDownIcon
+                                  className={classNames(
+                                    open ? 'text-gray-600' : 'text-gray-400',
+                                    'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              </Popover.Button>
+                              <Popover.Panel>
+                                <p className="mt-3 text-base text-gray-500">
+                                  {person.description && person.description}
+                                </p>
+                              </Popover.Panel>
+                            </>
+                          )}
+                        </Popover>
                       </div>
                     </div>
                   </div>
