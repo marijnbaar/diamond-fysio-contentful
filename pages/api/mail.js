eb7978c1bc0977e7aaecc581d28949268c5e0f61
Marijn Baar <marijnbaar@gmail.com>
2022-07-06 16:04:54 +0200

    added captcha to contact form

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,8 +2,28 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default function handler(req, res) {
+async function validateHuman(token) {
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
+
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    {
+      method: 'POST'
+    }
+  );
+  const data = await response.json();
+  return data.success;
+}
+
+export default async function handler(req, res) {
   const body = JSON.parse(req.body);
+
+  const human = await validateHuman(body.token);
+  if (!human) {
+    res.status(400);
+    res.json({ errors: 'You must be human to fill in this form' });
+    return;
+  }
   const message = `
     First Name: ${body.firstname}rn
     Last Name: ${body.lastname}rn
@@ -13,21 +33,18 @@ export default function handler(req, res) {
     Message: ${body.message}
     `;
 
+  const { subject } = body;
+
   const data = {
     to: 'diamondfysio@gmail.com',
     from: 'diamondfysio@gmail.com',
-    subject: 'New Message!',
+    subject: `${subject}!`,
     text: message,
-    html: message.replace(/\r\n/g, '<br>')
+    html: message.replace(/rn/g, '<br>')
   };
 
-  mail
-    .send(data)
-    // .then(() => {
-    //     console.log('Email sent')
-    // })
-    .catch((error) => {
-      console.error(error);
-    });
+  mail.send(data).catch((error) => {
+    console.error(error);
+  });
   res.status(200).json({ status: 'Ok' });
 }
