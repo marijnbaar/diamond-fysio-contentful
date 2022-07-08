950202ceb9c2df8f8d6ec36521eb6528b2b36a0d
Marijn Baar <marijnbaar@gmail.com>
2022-07-08 11:10:47 +0200

    added content page query and Google Map on contact page

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -61,7 +61,7 @@ export default function Contact({
     <div className="bg-white">
       <main className="overflow-hidden">
         {/* Header */}
-        <div className="bg-warm-gray-50">
+        <div className="bg-warm-gray-100">
           <div className="py-24 lg:py-32">
             <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
               <h1 className="text-4xl font-extrabold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">
