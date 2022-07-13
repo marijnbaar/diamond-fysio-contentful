a1c575fac796b5fb0368d119ca19b57c3c6a10a4
Marijn Baar <marijnbaar@gmail.com>
2022-07-13 13:15:40 +0200

    added team extra's homepage

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,4 +1,11 @@
+import { Popover } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
 export default function Team({ title, description, teamMemberCollection }) {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
@@ -13,65 +20,97 @@ export default function Team({ title, description, teamMemberCollection }) {
           >
             {teamMemberCollection &&
               teamMemberCollection.items.map((person) => (
-                <li key={person.name}>
-                  <div className="space-y-6">
-                    {person.image && (
-                      <img
-                        className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
-                        src={person.image.url}
-                        alt=""
-                      />
-                    )}
-                    <div className="space-y-2">
-                      <div className="text-lg leading-6 font-medium space-y-1">
-                        <h3>{person.name}</h3>
-                        <p className="text-teal-500">{person.role}</p>
+                <div
+                  key={person.name}
+                  className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+                >
+                  <li key={person.name}>
+                    <div className="space-y-6">
+                      {person.image && (
+                        <img
+                          className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                          src={person.image.url}
+                          alt=""
+                        />
+                      )}
+                      <div className="space-y-2 p-6 flex flex-col justify-between">
+                        <div className="text-lg leading-6 font-medium space-y-1">
+                          <h3>{person.name}</h3>
+                          <p className="text-teal-500">{person.role}</p>
+                          <Popover>
+                            {({ open }) => (
+                              <>
+                                <Popover.Button
+                                  className={classNames(
+                                    open ? 'text-gray-900' : 'text-gray-500',
+                                    'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-beige'
+                                  )}
+                                >
+                                  <span>Lees meer</span>
+                                  <ChevronDownIcon
+                                    className={classNames(
+                                      open ? 'text-gray-600' : 'text-gray-400',
+                                      'ml-2 h-5 w-5 group-hover:text-gray-500'
+                                    )}
+                                    aria-hidden="true"
+                                  />
+                                </Popover.Button>
+                                <Popover.Panel>
+                                  <p className="mt-3 text-base text-left text-gray-500">
+                                    {person.descriptionHomepage &&
+                                      documentToReactComponents(person.descriptionHomepage.json)}
+                                  </p>
+                                </Popover.Panel>
+                              </>
+                            )}
+                          </Popover>
+                        </div>
+                        <ul role="list" className="flex justify-center space-x-5">
+                          <li>
+                            {person.emailAddress && (
+                              <a
+                                href={person.emailAddress}
+                                className="text-gray-400 hover:text-gray-500"
+                              >
+                                <span className="sr-only">Twitter</span>
+                                <svg
+                                  className="w-5 h-5"
+                                  aria-hidden="true"
+                                  fill="currentColor"
+                                  viewBox="0 0 20 20"
+                                >
+                                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                                </svg>
+                              </a>
+                            )}
+                          </li>
+                          <li>
+                            {person.linkedinUrl && (
+                              <a
+                                href={person.linkedinUrl}
+                                className="text-gray-400 hover:text-gray-500"
+                              >
+                                <span className="sr-only">LinkedIn</span>
+                                <svg
+                                  className="w-5 h-5"
+                                  aria-hidden="true"
+                                  fill="currentColor"
+                                  viewBox="0 0 20 20"
+                                >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                                    clipRule="evenodd"
+                                  />
+                                </svg>
+                              </a>
+                            )}
+                          </li>
+                        </ul>
                       </div>
-                      <ul role="list" className="flex justify-center space-x-5">
-                        <li>
-                          {person.emailAddress && (
-                            <a
-                              href={person.emailAddress}
-                              className="text-gray-400 hover:text-gray-500"
-                            >
-                              <span className="sr-only">Twitter</span>
-                              <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                              >
-                                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                              </svg>
-                            </a>
-                          )}
-                        </li>
-                        <li>
-                          {person.linkedinUrl && (
-                            <a
-                              href={person.linkedinUrl}
-                              className="text-gray-400 hover:text-gray-500"
-                            >
-                              <span className="sr-only">LinkedIn</span>
-                              <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                                  clipRule="evenodd"
-                                />
-                              </svg>
-                            </a>
-                          )}
-                        </li>
-                      </ul>
                     </div>
-                  </div>
-                </li>
+                  </li>
+                </div>
               ))}
           </ul>
         </div>
