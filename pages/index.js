f91b3ebb14f1caf0c7bb364b0369638ac00cdf45
Marijn Baar <marijnbaar@gmail.com>
2022-07-15 12:52:21 +0200

    added instagram fetch

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,23 @@ import ComponentList from '../components/ComponentList';
 
 export const getStaticProps = async ({ preview = false }) => {
   const pageData = (await getPage('Homepage', undefined, preview)) ?? [];
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media,url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_API_KEY}`;
+  const data = await fetch(url);
+  const feed = await data.json();
+
   return {
     props: {
       ...pageData,
+      instagramPosts: feed,
       preview
     }
   };
 };
 
-const Home = ({ components }) => {
+const Home = (props) => {
   return (
     <div>
-      <ComponentList components={components} />
+      <ComponentList components={props.components} instagramPosts={props.instagramPosts} />
     </div>
   );
 };
