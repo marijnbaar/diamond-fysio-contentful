b5e6b4407a4c30f269cc60fa66bd592cef3482ab
Marijn Baar <marijnbaar@gmail.com>
2022-07-21 15:30:37 +0200

    fix contact form

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,28 +2,28 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// async function validateHuman(token) {
-//   const secret = process.env.RECAPTCHA_SECRET_KEY;
+async function validateHuman(token) {
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
 
-//   const response = await fetch(
-//     `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
-//     {
-//       method: 'POST'
-//     }
-//   );
-//   const data = await response.json();
-//   return data.success;
-// }
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    {
+      method: 'POST'
+    }
+  );
+  const data = await response.json();
+  return data.success;
+}
 
 export default async function handler(req, res) {
   const body = JSON.parse(req.body);
 
-  // const human = await validateHuman(req.token);
-  // if (!human) {
-  //   res.status(400);
-  //   res.json({ errors: 'You must be human to fill in this form' });
-  //   return;
-  // }
+  const human = await validateHuman(req.token);
+  if (!human) {
+    res.status(400);
+    res.json({ errors: 'You must be human to fill in this form' });
+    return;
+  }
   const message = `
     First Name: ${body.firstname}rn
     Last Name: ${body.lastname}rn
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
   const data = {
     to: 'diamondfysio@gmail.com',
     from: 'diamondfysio@gmail.com',
-    subject: `${subject}!`,
+    subject: `${subject}`,
     text: message,
     html: message.replace(/rn/g, '<br>')
   };
