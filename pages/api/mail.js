dbac4bedbd4d3af41b14962fd9450accceaff21f
Marijn Baar <marijnbaar@gmail.com>
2022-07-25 16:43:50 +0200

    added arrows slideshow

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
   const { subject } = body;
 
   const data = {
-    to: 'info@fysiodiamondfactory.com',
-    from: 'info@fysiodiamondfactory.com',
+    to: 'diamondfysio@gmail.com',
+    from: 'diamondfysio@gmail.com',
     subject: `${subject}`,
     text: message,
     html: message.replace(/rn/g, '<br>')
