15e4c1c021a000191db3651735028b3943bd1f10
Marijn Baar <marijnbaar@gmail.com>
2022-07-25 16:05:25 +0200

    refactor

--- components/Headers/TeampageHeader.jsx
+++ components/Headers/TeampageHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 function TeampageHeader({ title, description }) {
   return (
     <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:mt-24">
+      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 mt-24">
         <div className="space-y-12">
           <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
             <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">{title && title}</h2>
