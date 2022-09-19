3d164dd383640001cd19e6ae09a33484981546cf
Marijn Baar <marijnbaar@gmail.com>
2022-09-19 13:56:13 +0200

    refactoring and made quote font smaller on teampage

--- components/AboutInformation.jsx
+++ components/AboutInformation.jsx
@@ -6,17 +6,15 @@ export default function AboutInformation({ title, subtitle, description, aboutFe
   return (
     <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
+        <div className="text-center">
+          <h2 className="text-base mx-auto font-semibold tracking-wider text-cyan-600 uppercase">
             {subtitle}
           </h2>
-          <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+          <p className="mx-auto mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
             {title}
           </p>
-          <div>
-            <div className="prose mt-5 max-w-prose mx-auto text-xl text-gray-500">
-              {description && documentToReactComponents(description.json)}
-            </div>
+          <div className="prose mt-5 max-w-prose mx-auto text-xl text-gray-500">
+            {description && documentToReactComponents(description.json)}
           </div>
         </div>
         <div className="mt-10">
