8e8c37d27e322ec95390deec999dc605b281f4bb
Marijn Baar <marijn.baar@amsterdam.nl>
2025-03-16 10:09:41 +0100

    cleanup

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: 'https://www.fysiodiamondnijmegen.nl',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: ['https://www.fysiodiamondnijmegen.nl/sitemap.xml']
-  }
-};
